4f046c418b74979e38bc6def6bcc7cb7df6b70c3
Karol <karolkrawczyk8650@gmail.com>
2023-02-17 17:26:32 +0100

    small refactor

--- src/main/java/com/example/pressarticles/PressArticlesApplication.java
+++ src/main/java/com/example/pressarticles/PressArticlesApplication.java
@@ -1,26 +1,12 @@
 package com.example.pressarticles;
 
-import com.example.pressarticles.repositories.AuthorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // (scanBasePackages={"services","repositories", "entities", "controllers"})
+@SpringBootApplication
 
 public class PressArticlesApplication {
-
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(PressArticlesApplication.class, args);
     }
-
-    /*@EventListener(ApplicationReadyEvent.class)
-    public void init() {
-        Author author = new Author();
-        author.setFirstName("firstname");
-        author.setLastName("lastname");
-        authorRepository.save(author);
-    }*/
 }
--- src/main/java/com/example/pressarticles/services/PressArticleService.java
+++ src/main/java/com/example/pressarticles/services/PressArticleService.java
@@ -4,7 +4,6 @@ import com.example.pressarticles.entities.PressArticle;
 import com.example.pressarticles.repositories.PressArticleRepository;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
